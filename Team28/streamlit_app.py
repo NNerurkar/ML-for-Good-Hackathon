a8d346571a8111768dc74373293c0e96da1ba9ce
NNerurkar <nandini.nerurkar@morganstanley.com>
2021-11-24 17:06:57 +0000

    Add py script for streamlit app

--- Team28/streamlit_app.py
+++ Team28/streamlit_app.py
@@ -8,6 +8,22 @@ col1, col2, col3 = st.columns(3)
 with col1:
     st.header("Column1")
     st.image("https://static.streamlit.io/examples/cat.jpg")
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        # To read file as bytes:
+        bytes_data = uploaded_file.getvalue()
+        st.write(bytes_data)
+
+        # To convert to a string based IO:
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        st.write(stringio)
+        # To read file as string:
+        string_data = stringio.read()
+        st.write(string_data)
+
+        # Can be used wherever a "file-like" object is accepted:
+        dataframe = pd.read_csv(uploaded_file)
+        st.write(dataframe)
 
 with col2:
     st.header("Column 2")
