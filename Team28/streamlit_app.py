b8091972222e701ef835958e5d8b49595409519b
NNerurkar <nandini.nerurkar@morganstanley.com>
2021-12-02 19:42:08 +0000

    Change app to use expander

--- Team28/streamlit_app.py
+++ Team28/streamlit_app.py
@@ -1,36 +1,16 @@
 import streamlit as st
 import clustering as cl
+from chatbot import ChatBot as cb
 
 
-
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.header("Column1")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
-    uploaded_file = st.file_uploader("Choose a file")
-    if uploaded_file is not None:
-        # To read file as bytes:
-        bytes_data = uploaded_file.getvalue()
-        st.write(bytes_data)
-
-        # To convert to a string based IO:
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        st.write(stringio)
-        # To read file as string:
-        string_data = stringio.read()
-        st.write(string_data)
-
-        # Can be used wherever a "file-like" object is accepted:
-        dataframe = pd.read_csv(uploaded_file)
-        st.write(dataframe)
-
-with col2:
+with st.expander("Generate WordClouds using KMeans Clustering"):
     st.header("Generate WordClouds using KMeans Clustering")
-    #st.image("https://static.streamlit.io/examples/dog.jpg")
     cl.clustering_main()
 
-with col3:
-    st.header("Column 3")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
+with st.expander("Chatbot"):
+    st.header("How can I help you?")
+    bot_obj = cb()
+    
+    bot_obj.chatbot_main()
+
 
